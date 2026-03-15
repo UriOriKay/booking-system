4f4facf520f81b9a6b4894e04d1c350947005a43
UriOriKay <127621006+UriOriKay@users.noreply.github.com>
2026-03-15 19:41:12 +0100

    refactor(domain): add auditing and concurrency fields to BaseEntity

--- BookingSystem.Domain/Entities/BaseEntity.cs
+++ BookingSystem.Domain/Entities/BaseEntity.cs
@@ -4,5 +4,9 @@ namespace BookingSystem.Domain.Entities
     public abstract class BaseEntity
     {
         public Guid Id { get; set; }
+
+        public DateTime CreateAtUtc { get; set; }
+        public DateTime? UpdatedAtUtc { get; set; }
+        public byte[]? RowVersion { get; set; }
     }
 }
